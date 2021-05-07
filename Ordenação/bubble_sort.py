c0937fc40f5ca670a8c558098d1a744c30e7bc45
Fabio-Coder <0xfab1015@gmail.com>
2021-05-07 17:28:04 -0300

    Organizao o modulo bubble_sort.py

--- Ordenação/bubble_sort.py
+++ Ordenação/bubble_sort.py
@@ -1,19 +1,10 @@
-from Analise_de_complexidade.cronometro import cronometro, cronometro_ns
-
-
 def bubble_sort(lista):
-    """
-    Recebe uma lista de inteiros
-
-    :param lista:
-    :return:
-    """
     sentinela = False
     while not sentinela:
         sentinela = True
-        for i in range(len(lista)-1):
+        for i in range(len(lista) - 1):
             if i < len(lista):
-                if lista[i] > lista[i+1]:
+                if lista[i] > lista[i + 1]:
                     sentinela = False
-                    lista[i],lista[i+1]=lista[i+1],lista[i]
+                    lista[i], lista[i + 1] = lista[i + 1], lista[i]
     return lista
